004a8dc1e4c170e3649db4b8272f8896f278f2de
Paullohz <paullohenriquecastrosilva@gmail.com>
2024-01-10 23:56:58 -0300

    Agenda ruby

---
+++ agenda.rb
@@ -0,0 +1,94 @@
+#Adicionar, Editar e Remover Contato;
+#Contato terão as seguintes informações: nome e telefone;
+#Poderemos ver todos os contatos registrados ou somente um contato;
+
+@agenda = [
+  {nome:"Paullo", telefone:"999993333"},
+  {nome:"Fulano", telefone:"333339999"}
+]
+
+def todos_contatos
+  @agenda.each do |contato|
+    puts "#{contato[:nome]} - #{contato[:telefone]}"
+  end
+  puts "- - - - - - - - - -  - - - - - - - - - - -"
+end
+
+def ver_contato
+    print "Qual nome você deseja: "
+    nome = gets.chomp
+
+    @agenda.each do |contato|
+      if  contato[:nome].downcase.include?(nome.downcase)
+        puts "#{contato[:nome]} - #{contato[:telefone]}"
+      end
+    end
+    puts "- - - - - - - - - - - - - - - - - - - - - - - "
+end
+
+def adicionar_contato
+  print "Nome: "
+  nome = gets.chomp
+  print "Telefone: "
+  telefone = gets.chomp
+
+  @agenda << {nome: nome, telefone: telefone}
+end
+
+def editar_contato
+  print "Qual nome deseja editar: "
+  nome = gets.chomp
+
+  @agenda.each do |contato|
+    if contato[:nome].downcase == (nome.downcase)
+      print "Nome para editar (Se quiser manter o nome aperte enter.)"
+      nome_salvo = contato[:nome]
+
+      contato[:nome] = gets.chomp
+      contato[:nome] = contato[:nome].empty? ? nome_salvo : contato[:nome]
+
+      print "Telefone para editar (se quiser manter o mesmo nome, aperte enter.)"
+      telefone_salvo = contato[:telefone]
+
+      contato[:telefone] = gets.chomp
+      contato[:telefone] = contato[:telefone].empty? ? nome_salvo : contato[:telefone]
+    end
+  end
+end
+
+def remover_contato
+  print "Qual nome deseja remover: "
+  nome = gets.chomp
+
+  @agenda.each do |contato|
+    if contato[:nome].downcase == (nome.downcase)
+      indice = @agenda.index(contato)
+      @agenda.delete_at(indice)
+      break
+    end
+  end
+end
+
+loop do
+
+puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar contato\n5. Remover contato\n0. Sair"
+  codigo = gets.chomp.to_i
+
+  case
+  when codigo == 0
+    puts "Até mais!"
+    break
+  when codigo == 1
+    todos_contatos
+  when codigo == 2
+    adicionar_contato
+  when codigo == 3
+    ver_contato
+  when codigo == 4
+    editar_contato
+  when codigo == 5
+    remover_contato
+  else
+    puts "função não existe, use uma função válida."
+  end
+end
